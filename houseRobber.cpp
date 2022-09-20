af385881b6ccff56f3c1f4249f65921fce10a55a
Anikcsejnu <anikraju110@gmail.com>
2022-09-20 23:22:02 +0600

    Solve houseRobber with Iterative dp

--- houseRobber.cpp
+++ houseRobber.cpp
@@ -10,3 +10,21 @@ int solution(vector<int> nums) {
     vector<int> dp(nums.size() + 1, -1);
     return recur(nums, 0, dp);
 }
+
+
+// Solve with Iterative dp
+
+int solution(vector<int> nums) {
+    if(nums.size() == 0) return 0;
+    
+    int n = nums.size();
+    vector<int> dp(n + 1, -1);
+    dp[0] = nums[0];
+    dp[1]=max(nums[0], nums[1]);
+    
+    for(int i = 2;i <= n;i++) {
+        dp[i] = max(nums[i] + dp[i-2], dp[i-1]);
+    }
+    
+    return dp[n - 1];
+}
